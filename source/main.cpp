d5f6cd6eb0c5c7d68024e82ce5ea7c634be3682c
Jaeden Amero <jaeden@patater.com>
2006-10-31 15:58:59 -0700

    Added a dummy main.cpp
    Add a dummy {{{main.cpp}}} that will compile fine. Most people were
    just trying to compile my manual without making a {{{main.cpp}}}, so
    this will prevent them from having linker errors related to not having
    a {{{main}}} function.

---
+++ source/main.cpp
@@ -0,0 +1,20 @@
+/*
+ *  main.cpp
+ *  
+ *
+ *  Created by Jaeden Amero on 3/12/06.
+ *  Copyright 2006. All rights reserved.
+ *
+ */
+
+#include <nds.h>
+
+int main() {
+	// This is a blank loop just for you.
+	// You should follow along in the manual and fill in this file with useful code
+	// Good luck and have fun!
+	
+	for (;;) {
+		swiWaitForVBlank();
+	}
+}
