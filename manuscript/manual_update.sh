87be0500b8722487140330b63a452061eff52de3
Jaeden Amero <jaeden@patater.com>
2013-06-01 22:04:29 -0500

    Remove old manuscript updating script
    This updating script was used to bring in a new Apple Pages file,
    which was what the manual was originally written with. Since we use
    docbook now, this script isn't useful and can be deleted.

--- manuscript/manual_update.sh
+++
@@ -1,8 +0,0 @@
-#!bin/bash
-mv Manual.pages Manual4.pages
-svn up
-cp -R Manual.pages/thumbs/.svn Manual4.pages/thumbs/
-cp -R Manual.pages/Contents/.svn Manual4.pages/Contents/
-cp -R Manual.pages/.svn Manual4.pages/
-rm -rf Manual.pages
-mv Manual4.pages Manual.pages
